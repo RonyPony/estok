af36259e89aae0d638b75428fb951c5a13578b2b
Ronel Cruz <ronel.cruz.a8@gmail.com>
2026-09-10 14:20:15 -0400

    configuration

--- backend/src/eStok.Infrastructure/Persistence/Migrations/20260910154546_initialMigration.Designer.cs
+++ backend/src/eStok.Infrastructure/Persistence/Migrations/20260910154546_initialMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eStok.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using eStok.Infrastructure.Persistence;
 namespace eStok.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260910154546_initialMigration")]
+    partial class initialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
